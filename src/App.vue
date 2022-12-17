7787eb1c3fafe8831fe1d330e60cbe7dc7543336
alerrandro borges <alerrandrokaton@gmail.com>
2022-12-17 16:17:00 -0300

    Feat: route stack

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,27 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <header>
+    <img alt="icon" class="logo" src="@/assets/icon.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Alerrandro Borges" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Stacks</RouterLink>
+        <RouterLink to="/about">Experiência</RouterLink>
       </nav>
     </div>
   </header>
 
+  <main>
   <RouterView />
+  </main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  height: 100vh;
 }
 
 .logo {
@@ -39,13 +40,8 @@ nav {
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+  color: rgb(255, 255, 255);
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
 nav a {
   display: inline-block;
   padding: 0 1rem;
